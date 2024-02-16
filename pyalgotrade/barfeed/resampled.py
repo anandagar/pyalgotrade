9fa37c64d74952a3c8cfdd3e1ecf27b179f9dd08
Devdatta Thube <devdatta.thube@d10x.io>
2024-02-16 15:05:58 +0530

    logging edited

--- pyalgotrade/barfeed/resampled.py
+++ pyalgotrade/barfeed/resampled.py
@@ -77,9 +77,9 @@ class ResampledBarFeed(barfeed.BaseBarFeed):
             self.__range = resamplebase.build_range(dateTime, self.getFrequency())
             self.__grouper = BarsGrouper(self.__range.getBeginning(), value, self.getFrequency())
         elif self.__range.belongs(dateTime):
-            logging.warning(f"self.__range  arg datetime is {dateTime} begin{self.__range.getBeginning()} end {self.__range.getEnding()}")
             self.__grouper.addValue(value)
         else:
+            logging.warning(f"self.__range is {self.__range} datetime is {dateTime}")
             self.__values.append(self.__grouper.getGrouped())
             self.__range = resamplebase.build_range(dateTime, self.getFrequency())
             self.__grouper = BarsGrouper(self.__range.getBeginning(), value, self.getFrequency())
